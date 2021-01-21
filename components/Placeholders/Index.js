b9e2804e316596feccaf8c32ca39c0907e6231fd
Tzivi <gelsteintzivisantiago@gmail.com>
2021-01-21 08:23:50 -0300

    Fix 002: production error. Module AvatarPlaceholder not found

--- components/Placeholders/Index.js
+++ components/Placeholders/Index.js
@@ -1,29 +1,10 @@
 import styled from '@emotion/styled'
+import styles from './styles.module.css'
 
-export const AvatarPlaceholder = styled.div`
-  width: 28px;
-  height: 28px;
-  border-radius: 50%;
-  animation: reflect 1s linear infinite;
-  background: -webkit-gradient(
-    linear,
-    left top,
-    right top,
-    color-stop(8%, #bebebe),
-    color-stop(18%, #fff),
-    color-stop(33%, #bebebe)
-  );
-  background: -webkit-linear-gradient(left, #bebebe 8%, #fff 18%, #bebebe 33%);
-  background: linear-gradient(to right, #bebebe 8%, #fff 18%, #bebebe 33%);
-  -webkit-background-size: 300% 32px;
-  transform: rotate(135deg);
-
-  @keyframes reflect {
-    0% {
-      background-position: -100% 0;
-    }
-    100% {
-      background-position: 100% 0;
-    }
-  }
-`
+export const AvatarPlaceholder = ({ width = '28px' }) => {
+  const Avatar = styled.div`
+    width: ${width};
+    height: ${width};
+  `
+  return <Avatar className={styles.avatar}></Avatar>
+}
--- pages/compose/tweet/index.js
+++ pages/compose/tweet/index.js
@@ -5,7 +5,7 @@ import { ArrowLeftIcon } from 'components/Icons'
 import appContext from 'context/app/appContext'
 import Spinner from 'components/Spinner/Spinner'
 import { ExternalContainer, Mobile } from 'components/Globals'
-import { AvatarPlaceholder } from '../../../components/Placeholders'
+import { AvatarPlaceholder } from 'components/Placeholders'
 import useUser from 'hooks/useUser'
 import { useRouter } from 'next/router'
 
@@ -141,7 +141,7 @@ const Index = () => {
           </Header>
           <NewTweeetContainer>
             <PictureContainer>
-              {user ? <Picture src={user.photoURL} /> : <AvatarPlaceholder style={{ width: '46px', height: '46px' }} />}
+              {user ? <Picture src={user.photoURL} /> : <AvatarPlaceholder width="46px" />}
             </PictureContainer>
             <TextArea
               maxLength={`${MAX_CHAR}`}
