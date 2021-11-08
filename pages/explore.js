29528d4bd7b22c3f4d0c75f19dee9d9dd2165a57
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-11-08 11:41:43 -0300

    Delete unused vars

--- pages/explore.js
+++ pages/explore.js
@@ -3,15 +3,15 @@ import Layout from 'components/Layout'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import useUser from 'hooks/useUser'
-import styled from '@emotion/styled'
 
 const Explore = () => {
   const router = useRouter()
-
   const user = useUser()
+
   useEffect(() => {
     user === null && router.replace('/login')
   }, [user])
+
   const layoutConfig = {
     title: 'Explore',
     home: false,
