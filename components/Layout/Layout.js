adf764ceac152f4806caa070939e7a7b30c6308e
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-05-26 15:18:19 -0300

    add styles for layout and refactor layout

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,13 +1,16 @@
 import Header from 'components/Header'
 import Nav from 'components/Nav'
+import styles from './index.module.css'
+import Sidebar from 'components/Sidebar'
 
 const Layout = ({ children, layoutConfig }) => {
   return (
-    <>
+    <div className={styles.container}>
       <Header layoutConfig={layoutConfig} />
+      <Sidebar />
       {children}
       <Nav layoutConfig={layoutConfig} />
-    </>
+    </div>
   )
 }
 
