e0a9d9c87ff63f3a50b6dd76984f52f65897735e
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-05-26 15:18:30 -0300

    remove mobile container

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import { ExternalContainer, Mobile } from 'components/Globals'
+import { AppContainer } from 'components/Globals'
 import AppState from 'context/app/appState'
 import AuthState from 'context/auth/authState'
 import Head from 'next/head'
@@ -8,14 +8,12 @@ const MyApp = ({ Component, pageProps }) => {
   return (
     <AuthState>
       <AppState>
-        <ExternalContainer>
-          <Mobile>
-            <Head>
-              <link rel="icon" href="/resources/favicon.png" />
-            </Head>
-            <Component {...pageProps} />
-          </Mobile>
-        </ExternalContainer>
+        <AppContainer>
+          <Head>
+            <link rel="icon" href="/resources/favicon.png" />
+          </Head>
+          <Component {...pageProps} />
+        </AppContainer>
       </AppState>
     </AuthState>
   )
